574a1e25a6a5ed75c882a656d27c27311ec99e68
WassimAjili <wassim-ajili@hotmail.fr>
2017-12-11 11:46:10 +0100

    Modification bdd

--- ressources/script/pictureme.sql
+++ ressources/script/pictureme.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  mer. 29 nov. 2017 à 15:17
+-- Généré le :  lun. 11 déc. 2017 à 10:42
 -- Version du serveur :  5.7.19
 -- Version de PHP :  5.6.31
 
@@ -52,19 +52,23 @@ INSERT INTO `photos` (`mail`, `image`) VALUES
 
 DROP TABLE IF EXISTS `utilisateur`;
 CREATE TABLE IF NOT EXISTS `utilisateur` (
+  `id` int(32) NOT NULL AUTO_INCREMENT,
   `AdresseMail` varchar(25) NOT NULL,
   `MotDePasse` text NOT NULL,
   `Nom` varchar(25) NOT NULL,
-  `Prénom` varchar(25) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `Prénom` varchar(25) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `utilisateur`
 --
 
-INSERT INTO `utilisateur` (`AdresseMail`, `MotDePasse`, `Nom`, `Prénom`) VALUES
-('test@gmail.com', 'tested', 'kana', 'thithy'),
-('thithy@gmail.com', 'dgdgf', 'tatoo', 'thth');
+INSERT INTO `utilisateur` (`id`, `AdresseMail`, `MotDePasse`, `Nom`, `Prénom`) VALUES
+(1, 'test@gmail.com', 'tested', 'kana', 'thithy'),
+(2, 'thithy@gmail.com', 'dgdgf', 'tatoo', 'thth'),
+(3, 'wassim-ajili@hotmail.fr', '$2a$10$vD44uOPk4k56wdznTrGgdOvWX8TTAjiT5uvo/wM.oxvK376.tloiy', 'Wassim Ajili', 'Wassim Ajili');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
